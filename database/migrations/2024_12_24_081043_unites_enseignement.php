1ae7e8d2b746c8ee8215ab9fed4d5bae5c4a82e3
ASSONGBA Méchac <mechacassongba68@gmail.com>
2024-12-24 09:33:49 +0100

    [Chef] Création des tables pour les UEs et ECs

---
+++ database/migrations/2024_12_24_081043_unites_enseignement.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('unites_enseignement', function (Blueprint $table) {
+            $table->id(); 
+            $table->string('code')->unique(); // Code unique de l'UE (ex: UE11)
+            $table->string('nom'); 
+            $table->integer('credits_ects'); 
+            $table->tinyInteger('semestre')->unsigned(); 
+            $table->timestamps(); // Champs created_at et updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('unites_enseignement');
+    }
+};
