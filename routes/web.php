aea6f7eba56d84ee6dada67ef7e13f9dc602aa16
ASSONGBA Méchac <mechacassongba68@gmail.com>
2024-12-25 13:28:05 +0100

    [Chef] Création des pages pour gérer les UEs et ECs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UEsController;
+use App\Http\Controllers\ECsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/UEs', [UEsController::class, 'index'])->name('UEs.index');
+
+Route::resource('UEs', UEsController::class);
+Route::resource('ECs', ECsController::class);
