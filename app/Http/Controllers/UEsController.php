1b91bfa06862041fafb95fbaa671ff9008c236fb
ASSONGBA Méchac <mechacassongba68@gmail.com>
2024-12-25 13:28:05 +0100

    [Chef] Création des pages pour gérer les UEs et ECs

--- app/Http/Controllers/UEsController.php
+++ app/Http/Controllers/UEsController.php
@@ -11,7 +11,7 @@ class UEsController extends Controller
      */
     public function index()
     {
-        //
+        return view('UEs.index');
     }
 
     /**
---
+++ resources/views/UEs/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Liste des UEs')
+
+@section('content')
+<div class="container mx-auto">
+    <h1 class="text-2xl font-bold mb-4">Liste des Unités d’Enseignement</h1>
+
+    <p>Bienvenue dans la page des UEs !</p>
+
+    <a href="#" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Créer une nouvelle UE</a>
+</div>
+@endsection
